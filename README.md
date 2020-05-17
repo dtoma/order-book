bc80c7949a92cb496a51644cf46cdc09c86b3479
Damien Toma <damientoma.pro@gmail.com>
2020-05-17 15:34:36 +0800

    Remove unused dependency & improve console output

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Order Book
 
 ![](https://github.com/dtoma/order-book/workflows/CI/badge.svg)
+
+## Build
+
+The project is built using CMake, so we can build/test/run with portable commands.
+
+Example commands can be found in [the CI configuration file](.github/workflows.cpp.yml).
--- book/src/main.cpp
+++ book/src/main.cpp
@@ -15,15 +15,26 @@ int main(int ac, char** av) {
     spdlog::info("Read {} orders", orders.size());
 
     ob::OrderBook order_book;
+    spdlog::info("Initial order book:");
+    order_book.show_bids();
+    order_book.show_asks();
     for (auto& type_and_order : orders) {
+        spdlog::info("\n");
+        auto& order = type_and_order.second;
+        spdlog::info("Load order id={}", order.id);
+
+        if (type_and_order.first == ob::OrderType::NEW) {
+            order_book.place_order(order);
+        } else if (type_and_order.first == ob::OrderType::CANCEL) {
+            order_book.cancel(order);
+        }
+
+        spdlog::info("Order book is now:");
         order_book.show_bids();
         order_book.show_asks();
-        if (type_and_order.first == ob::OrderType::NEW) {
-            order_book.place_order(type_and_order.second);
-        } else if (type_and_order.first == ob::OrderType::CANCEL) {
-            order_book.cancel(type_and_order.second);
-        }
     }
+
+    spdlog::info("\n");
 
     spdlog::info("Final order book:");
     order_book.show_bids();
